424b327ef5e0752e9e87351d3a14e777f082b40f
BoYeRI <erik.wallen3@gmail.com>
2025-03-12 11:04:01 +0100

    Update App.jsx

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import WeatherApp from "./components/Weather/WeatherApp";
+import WeatherG from "./components/Weather/WeatherG";
 
 const App = () => {
   return (
     <div className="WeatherSite">
       <WeatherApp/>
+      <WeatherG/>
     </div>
   );
 };
